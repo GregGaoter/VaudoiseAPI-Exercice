12cdcd8c3686e6634ee92ee8fadc3b328013a805
GregGaoter <greg.gautier@hotmail.com>
2025-10-27 11:09:16 +0100

    Refactor ContractSpecifications to use constant fields for property access

--- src/main/java/ch/vaudoise/vaudoiseapi/exercice/service/specification/ContractSpecifications.java
+++ src/main/java/ch/vaudoise/vaudoiseapi/exercice/service/specification/ContractSpecifications.java
@@ -1,6 +1,8 @@
 package ch.vaudoise.vaudoiseapi.exercice.service.specification;
 
+import ch.vaudoise.vaudoiseapi.exercice.domain.Company_;
 import ch.vaudoise.vaudoiseapi.exercice.domain.Contract;
+import ch.vaudoise.vaudoiseapi.exercice.domain.Contract_;
 import java.time.Instant;
 import java.util.UUID;
 import org.springframework.data.jpa.domain.Specification;
@@ -8,22 +10,22 @@ import org.springframework.data.jpa.domain.Specification;
 public class ContractSpecifications {
 
     public static Specification<Contract> hasCompanyId(UUID companyId) {
-        return (root, query, cb) -> cb.equal(root.get("company").get("id"), companyId);
+        return (root, query, cb) -> cb.equal(root.get(Contract_.COMPANY).get(Company_.ID), companyId);
     }
 
     public static Specification<Contract> isActive() {
-        return (root, query, cb) -> cb.greaterThan(root.get("endDate"), Instant.now());
+        return (root, query, cb) -> cb.greaterThan(root.get(Contract_.END_DATE), Instant.now());
     }
 
     public static Specification<Contract> updatedAfter(Instant fromDate) {
-        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("updateDate"), fromDate);
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get(Contract_.UPDATE_DATE), fromDate);
     }
 
     public static Specification<Contract> updatedBefore(Instant toDate) {
-        return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("updateDate"), toDate);
+        return (root, query, cb) -> cb.lessThanOrEqualTo(root.get(Contract_.UPDATE_DATE), toDate);
     }
 
     public static Specification<Contract> updatedBetween(Instant fromDate, Instant toDate) {
-        return (root, query, cb) -> cb.between(root.get("updateDate"), fromDate, toDate);
+        return (root, query, cb) -> cb.between(root.get(Contract_.UPDATE_DATE), fromDate, toDate);
     }
 }
